8a44b316cf91b5904b2ddc239c6770227de38959
Daanshaw <74239386+Daanshaw@users.noreply.github.com>
2022-12-01 14:51:27 +0000

    DONE!!!

--- src/ApplicationTier.java
+++ src/ApplicationTier.java
@@ -18,8 +18,7 @@ public class ApplicationTier {
 
     void applicationTier() throws Exception {
 
-
-        // Create a Scanner object
+        PresentationTier pt = new PresentationTier();
 
 
         int option = scanner.nextInt();  // Read user input
@@ -47,29 +46,18 @@ public class ApplicationTier {
                 PatientInterface patient = (PatientInterface) registry.lookup("patient");
 
                 // Display the current student details
-                System.out.println("Patient details:");
-                System.out.println("Name: " + patient.getName());
-                System.out.println("Surname: " + patient.getSurname());
-                System.out.println("NHS number: " + patient.getNhsRegistrationNo());
-                System.out.println("Address: " + patient.getAddress());
-                System.out.println("Medical condition: " + patient.getMedicalCondition());
 
-//			// Change the student details
-//			System.out.print("Enter new student name: ");
-//			String name = input.readLine();
-//			patient.setName(name);
-//			System.out.print("Enter new student matric: ");
-//			String matric = input.readLine();
-//			patient.setSurname(matric);
-//			System.out.print("Enter new student programme: ");
-//			String programme = input.readLine();
-//			patient.setNhsRegistrationNo(Integer.parseInt(programme));
+
+                pt.patientDetailsMsg(patient.getName(), patient.getSurname(), patient.getNhsRegistrationNo(), patient.getAddress(),patient.getMedicalCondition() );
+
+
+
             } catch (IOException ioe) {
-                System.err.println("Error in I/O");
+                pt.IOMsg();
                 System.err.println(ioe.getMessage());
                 System.exit(-1);
             } catch (NotBoundException nbe) {
-                System.err.println("Object not declared on RMI Registry");
+                pt.noDataMsg();
                 System.err.println(nbe.getMessage());
                 System.exit(-1);
             }
@@ -78,12 +66,7 @@ public class ApplicationTier {
     }
 
 
-//    DataTier dt = new DataTier();
-////    void sendCallout() throws SQLException {
-////
-////        dt.createTable();
-////
-////    }
+
 
     void sendCalloutDetails() {
         PresentationTier pt = new PresentationTier();
@@ -109,19 +92,15 @@ public class ApplicationTier {
 
         Callout callout = new Callout(name, surname, nhsRegNo, accountDesc, timestamp, location, actionTakenDesc, callTimeSec);
 
-        System.out.println(callout);
 
 
-        //sendCallout();
-        //dt.insertCallout(callout);
+
 
         try {
-            // Set up the keyboard input
+
 
             String ip = "192.168.0.3";
 
-            // Create a new student object
-            //Patient patient = new Patient("John Dann", "01234567", 123, "1231", "sikc");
 
             // Create the remote version of the student object
             CalloutInterface callout_stub = (CalloutInterface) UnicastRemoteObject.exportObject(callout, 0);
@@ -131,9 +110,9 @@ public class ApplicationTier {
 
             // Declare the object with the registry
             registry.rebind("callout", callout_stub);
-            System.out.println("Callout bound");
+
         } catch (Exception e) {
-            System.err.println("Error Occurred");
+
             System.err.println(e.getMessage());
             System.exit(-1);
         }
--- src/PresentationTier.java
+++ src/PresentationTier.java
@@ -1,63 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.rmi.NotBoundException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.sql.SQLException;
-import java.util.Scanner;
-
 public class PresentationTier {
 
     ApplicationTier ap = new ApplicationTier();
 
     void presentationTier() throws Exception {
 
-        while(true) {
+        while (true) {
 
             System.out.println("Welcome to KWIK MEDICAL SYSTEM - Ambulance");
             System.out.println("Choose form one of the following options:");
             System.out.println("");
             System.out.println("1. Check for any patient info sent from hospital.");
-            System.out.println("2. Check for rescue requests.");
-            System.out.println("3. Enter and send callout details to the KMS.");
+            System.out.println("3. Enter and send callout details to the hospital.");
             System.out.println("4. Exit program.");
 
             ap.applicationTier();
         }
 
 
-
-
     }
 
 
-    void enterNameMsg(){
+    void enterNameMsg() {
 
         System.out.println("Enter patient's name:");
     }
-    void enterSurnameMsg(){
+
+    void enterSurnameMsg() {
 
         System.out.println("Enter patient's surname:");
     }
-    void enterNhsNoMsg(){
+
+    void enterNhsNoMsg() {
 
         System.out.println("Enter patient's NHS registration number:");
     }
-    void enterAccDescMsg(){
+
+    void enterAccDescMsg() {
 
         System.out.println("Enter patient's accident description:");
     }
-    void enterLocationMsg(){
+
+    void enterLocationMsg() {
 
         System.out.println("Enter accident's location:");
     }
-    void enterActionTakenDesc(){
+
+    void enterActionTakenDesc() {
 
         System.out.println("Enter description of action taken:");
     }
-    void enterCallTimeMsg(){
+
+
+    void enterCallTimeMsg() {
 
         System.out.println("Enter call time:");
     }
+
+    void patientDetailsMsg(String name, String surname, int nhsNo, String address, String medicalCond) {
+
+        System.out.println(
+                "Name:'" + name + '\'' +
+                        ", Surname:'" + surname + '\'' +
+                        ", NHS Registration Number:" + nhsNo +
+                        ", Address:'" + address + '\'' +
+                        ", Medical Condition:'" + medicalCond);
+    }
+
+    void IOMsg() {
+
+        System.err.println("Error in I/O");
+    }
+
+    void noDataMsg() {
+
+        System.err.println("No available data show.");
+    }
+
+    void errorOcMsg() {
+
+        System.err.println("Error Occurred");
+    }
+
+
 }
